19a5046f695de0cc41a69ef312e2ab9ea275742a
i-wolffe <adamantwolf@hotmail.com>
2019-08-03 02:48:10 -0500

    Reset size

--- js/dynamic_gallery.js
+++ js/dynamic_gallery.js
@@ -2,7 +2,7 @@
 function startOff() {
     $('.boxframe1').removeClass('openFrame');
     $('.slidebox').css('width','100px');
-    $('.img-b').removeClass('fullshine');
+    $('#img-b').removeClass('fullshine');
     var thewidth = $('#firstRowImages').contents('.size1:nth-child(2)').contents().width();
     $('.size1:nth-child(2)').width(thewidth);
     $('.size1:nth-child(2)').contents().addClass('fullshine');
@@ -13,10 +13,9 @@ function startOff() {
     $('.gallimg').hover(function switchimg(){
       var thewidth = (this).width;
     $('.slidebox').css('width','100px');
-    $('.img-b').removeClass('fullshine');
+    $('#img-b').removeClass('fullshine');
     $('.boxframe1').removeClass('openFrame');
     $(this).addClass('fullshine').parent('div').width(thewidth).parent().addClass('openFrame');
 
     });
     startOff();
-    
