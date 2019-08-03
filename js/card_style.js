bb6924d9e03ec3dd988cc24431c83c402cdf2516
i-wolffe <adamantwolf@hotmail.com>
2019-08-03 05:23:02 -0500

    Added another card proposal

---
+++ js/card_style.js
@@ -0,0 +1,9 @@
+$('.expand-btn-1').click(function() {
+
+	$('.role-description').toggleClass('expand');
+	$('.expand-btn-1').toggleClass('move-button');
+	$('.image-card-1').toggleClass('color-reveal');
+	$('.team-info').toggleClass('text-reduce');
+	$('.team-info-des').toggleClass('text-reveal-1');
+
+});
